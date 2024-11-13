d46fa6a4d9da91035a33b0bd7283cc6c29f5e2a1
chanho0908 <uni070@naver.com>
2024-11-13 12:05:49 +0900

    initialized commit

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    dependencies{
+        classpath(libs.navigation.safe.args)
+        classpath(libs.kotlin.serialization.plugin)
+    }
+}
+
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.jetbrains.kotlin.jvm) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.kapt) apply false
+    alias(libs.plugins.navigation.safe.args) apply false
+    alias(libs.plugins.kotlin.parcelize) apply false
+}
