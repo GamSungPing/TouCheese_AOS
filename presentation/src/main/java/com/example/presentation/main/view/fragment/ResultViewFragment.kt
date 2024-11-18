e04f488179301d3f6ef0973bff86c01ff0f6072e
joy293 <opendoor2026@gmail.com>
2024-11-18 15:58:06 +0900

    HomeResultView Fragment 추가

---
+++ presentation/src/main/java/com/example/presentation/main/view/fragment/ResultViewFragment.kt
@@ -0,0 +1,28 @@
+package com.example.presentation.main.view.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.presentation.R
+import com.example.presentation.databinding.FragmentResultViewBinding
+
+class ResultViewFragment : Fragment() {
+    private var _binding : FragmentResultViewBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentResultViewBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+}
