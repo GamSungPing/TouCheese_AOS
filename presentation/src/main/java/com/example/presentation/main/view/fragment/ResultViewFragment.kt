2abbe1a218b2fc28753952c92391370daf723195
joy293 <opendoor2026@gmail.com>
2024-11-19 19:41:47 +0900

    feature 가격순 필터 옵션 선택 기능

--- presentation/src/main/java/com/example/presentation/main/view/fragment/ResultViewFragment.kt
+++ presentation/src/main/java/com/example/presentation/main/view/fragment/ResultViewFragment.kt
@@ -4,18 +4,60 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupWindow
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.presentation.databinding.FragmentResultViewBinding
+import com.example.presentation.databinding.PriceFilterPopupBinding
+import com.example.presentation.databinding.RegionFilterPopupBinding
 import com.example.presentation.sample.ResultViewAdapter
+import com.example.presentation.sample.ResultViewModel
 import com.example.presentation.sample.Studio
 
 class ResultViewFragment : Fragment() {
     private var _binding : FragmentResultViewBinding? = null
     private val binding get() = _binding!!
 
+    private var _priceFilterBinding: PriceFilterPopupBinding? = null
+    private val priceFilterBinding get() = _priceFilterBinding!!
+
     private lateinit var resultViewAdapter: ResultViewAdapter
+    private val viewModel: ResultViewModel by viewModels()
+
+    // 임시 데이터
+    val sampleData = listOf(
+        Studio(id = 1, name = "Studio 1", listOf(
+            "image1",
+            "image2",
+            "image3",
+            "image4",
+            "image5",
+        )),
+        Studio(id = 2, name = "Studio 2", listOf(
+            "image1",
+            "image2",
+            "image3",
+            "image4",
+            "image5",
+        )),
+        Studio(id = 3, name = "Studio 3", listOf(
+            "image1",
+            "image2",
+            "image3",
+            "image4",
+            "image5",
+        )),
+        Studio(id = 4, name = "Studio 4", listOf(
+            "image1",
+            "image2",
+            "image3",
+            "image4",
+            "image5",
+        ))
+    )
 
 
     override fun onCreateView(
@@ -24,6 +66,8 @@ class ResultViewFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentResultViewBinding.inflate(inflater, container, false)
+        _priceFilterBinding = PriceFilterPopupBinding.inflate(layoutInflater)
+
         return binding.root
     }
 
@@ -31,44 +75,17 @@ class ResultViewFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         setupRvStudioList()
+        setupPriceFilterPopup()
+        setupRegionFilterPopup()
 
-        // 임시 데이터
-        val sampleData = listOf(
-            Studio(id = 1, name = "Studio 1", listOf(
-                "image1",
-                "image2",
-                "image3",
-                "image4",
-                "image5",
-            )),
-            Studio(id = 2, name = "Studio 2", listOf(
-                "image1",
-                "image2",
-                "image3",
-                "image4",
-                "image5",
-            )),
-            Studio(id = 3, name = "Studio 3", listOf(
-                "image1",
-                "image2",
-                "image3",
-                "image4",
-                "image5",
-            )),
-            Studio(id = 4, name = "Studio 4", listOf(
-                "image1",
-                "image2",
-                "image3",
-                "image4",
-                "image5",
-            ))
-        )
+        observeViewModel()
 
         resultViewAdapter.submitList(sampleData)
     }
 
     override fun onDestroyView() {
         _binding = null
+        _priceFilterBinding = null
         super.onDestroyView()
     }
 
@@ -78,6 +95,64 @@ class ResultViewFragment : Fragment() {
             layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
             addItemDecoration(DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL))
             adapter = resultViewAdapter
+        }
+    }
+
+    private fun setupPriceFilterPopup() {
+        val popupWindow = PopupWindow(
+            priceFilterBinding.root,
+            ConstraintLayout.LayoutParams.WRAP_CONTENT,
+            ConstraintLayout.LayoutParams.WRAP_CONTENT,
+            true
+        )
+
+        binding.btFilterPrice.apply {
+            setOnClickListener {
+                popupWindow.showAsDropDown(this)
+
+                priceFilterBinding.apply {
+                    btPriceAll.setOnClickListener {
+                        viewModel.setSelectedButton("btPriceAll")
+                    }
+                    btPrice1.setOnClickListener {
+                        viewModel.setSelectedButton("btPrice1")
+                    }
+                    btPrice2.setOnClickListener {
+                        viewModel.setSelectedButton("btPrice2")
+                    }
+                    btPrice3.setOnClickListener {
+                        viewModel.setSelectedButton("btPrice3")
+                    }
+                }
+            }
+        }
+    }
+
+    private fun setupRegionFilterPopup() {
+        val popupBinding = RegionFilterPopupBinding.inflate(layoutInflater)
+
+        val popupWindow = PopupWindow(
+            popupBinding.root,
+            ConstraintLayout.LayoutParams.WRAP_CONTENT,
+            ConstraintLayout.LayoutParams.WRAP_CONTENT,
+            true
+        )
+
+        binding.btFilterRegion.apply {
+            setOnClickListener {
+                popupWindow.showAsDropDown(this)
+            }
+        }
+    }
+
+    private fun observeViewModel() {
+        viewModel.selectedButton.observe(viewLifecycleOwner) { selectedButton ->
+            priceFilterBinding.apply {
+                btPriceAll.isChecked = (selectedButton == "btPriceAll")
+                btPrice1.isChecked = (selectedButton == "btPrice1")
+                btPrice2.isChecked = (selectedButton == "btPrice2")
+                btPrice3.isChecked = (selectedButton == "btPrice3")
+            }
         }
     }
 }
--- presentation/src/main/java/com/example/presentation/sample/ResultViewModel.kt
+++ presentation/src/main/java/com/example/presentation/sample/ResultViewModel.kt
@@ -1,4 +1,18 @@
 package com.example.presentation.sample
 
-class ResultViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ResultViewModel : ViewModel() {
+    private val _selectedButton = MutableLiveData<String?>()
+    val selectedButton: LiveData<String?> get() = _selectedButton
+
+    fun setSelectedButton(selectedButton: String) {
+        if (_selectedButton.value == selectedButton) {
+            _selectedButton.value = null
+        } else {
+            _selectedButton.value = selectedButton
+        }
+    }
 }
