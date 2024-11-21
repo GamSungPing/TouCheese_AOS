3be19a585944dcab373ac89f81a5f02d9f8a44ef
chanho0908 <uni070@naver.com>
2024-11-21 13:40:01 +0900

    feat : 바텀 네비게이션 프래그먼트 추가

--- presentation/src/main/java/com/example/presentation/main/view/fragment/HomeConceptFragment.kt
+++ presentation/src/main/java/com/example/presentation/main/view/fragment/HomeConceptFragment.kt
@@ -1,35 +1,46 @@
 package com.example.presentation.main.view.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import com.example.domain.rule.Concept
 import com.example.presentation.R
 import com.example.presentation.databinding.FragmentHomeConceptBinding
-import com.example.presentation.main.vm.MainHomeViewModel
+import com.example.presentation.main.vm.HomeConceptViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeConceptFragment : Fragment(R.layout.fragment_home_concept) {
-    private val viewModel: MainHomeViewModel by viewModels()
+    private val viewModel: HomeConceptViewModel by activityViewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val binding = FragmentHomeConceptBinding.bind(view)
-        viewModel
-    }
 
-    private fun showFragment(fragment: Fragment) {
-        childFragmentManager.beginTransaction().apply {
-            show(fragment)
-            commit()
-        }
-    }
-
-    private fun hideFragment(fragment: Fragment) {
-        childFragmentManager.beginTransaction().apply {
-            hide(fragment)
-            commit()
+        with(binding){
+            btnConceptLiveliness.setOnClickListener {
+                viewModel.onChangeScreenState(Concept.Liveliness)
+            }
+            btnConceptFlash.setOnClickListener {
+                viewModel.onChangeScreenState(Concept.FlashIdol)
+            }
+            btnConceptActor.setOnClickListener {
+                viewModel.onChangeScreenState(Concept.Actor)
+            }
+            btnConceptNatural.setOnClickListener {
+                viewModel.onChangeScreenState(Concept.Natural)
+            }
+            btnConceptDoll.setOnClickListener {
+                viewModel.onChangeScreenState(Concept.Doll)
+            }
+            btnConceptWatercolor.setOnClickListener {
+                viewModel.onChangeScreenState(Concept.WaterColor)
+            }
         }
     }
 }
--- presentation/src/main/java/com/example/presentation/main/view/fragment/HomeConceptMainFragment.kt
+++ presentation/src/main/java/com/example/presentation/main/view/fragment/HomeConceptMainFragment.kt
@@ -1,16 +1,34 @@
 package com.example.presentation.main.view.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.NavHostFragment
+import com.example.domain.rule.Concept
 import com.example.presentation.R
 import com.example.presentation.databinding.FragmentHomeConceptMainBinding
+import com.example.presentation.main.vm.HomeConceptViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class HomeConceptMainFragment : Fragment(R.layout.fragment_home_concept_main) {
+    private val viewModel: HomeConceptViewModel by activityViewModels()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val binding = FragmentHomeConceptMainBinding.bind(view)
+
+        val navController = NavHostFragment.findNavController(
+            childFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        )
+
+        viewModel.uiState.observe(viewLifecycleOwner) { state ->
+            val action = HomeConceptFragmentDirections.actionHomeConceptFragmentToTestFragment(state.id)
+            navController.navigate(action)
+        }
     }
 }
