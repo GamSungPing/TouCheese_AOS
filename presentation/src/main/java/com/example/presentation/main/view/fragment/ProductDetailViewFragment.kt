1263c91e00190ead12b392b91af73ac6951d2a9d
joy293 <opendoor2026@gmail.com>
2024-11-29 14:37:27 +0900

    feat : 상품 상세페이지 UI 구현완료

--- presentation/src/main/java/com/example/presentation/main/view/fragment/ProductDetailViewFragment.kt
+++ presentation/src/main/java/com/example/presentation/main/view/fragment/ProductDetailViewFragment.kt
@@ -1,23 +1,24 @@
 package com.example.presentation.main.view.fragment
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.presentation.R
 import com.example.presentation.databinding.FragmentProductDetailViewBinding
 
-class ProductDetailViewFragment : Fragment(R.layout.fragment_product_detail_view) {
+class ProductDetailViewFragment : Fragment() {
 
     private var _binding: FragmentProductDetailViewBinding? = null
     private val binding get() = _binding!!
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        _binding = FragmentProductDetailViewBinding.bind(view)
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentProductDetailViewBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
