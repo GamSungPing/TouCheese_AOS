656544b26f6045ea56122c79bc641974d33f6011
joy293 <opendoor2026@gmail.com>
2024-11-29 15:59:37 +0900

    feat : default Locale 설정

--- presentation/src/main/java/com/example/presentation/main/view/MainActivity.kt
+++ presentation/src/main/java/com/example/presentation/main/view/MainActivity.kt
@@ -1,15 +1,15 @@
 package com.example.presentation.main.view
 
+import android.content.Context
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.os.LocaleList
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.presentation.R
 import com.example.presentation.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.Locale
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -25,6 +25,16 @@ class MainActivity : AppCompatActivity() {
 
         val bottomNavigationView = binding.bottomNavigation
         bottomNavigationView.setupWithNavController(navController)
-    }
 
+        settingLocale(this)
+    }
+}
+
+fun settingLocale(context: Context) {
+    val locale = Locale("ko", "KR")
+    Locale.setDefault(locale)
+
+    val localeList = LocaleList(locale)
+    val config = context.resources.configuration
+    config.setLocales(localeList)
 }
