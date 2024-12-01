46308f4a53e2831d802d37596d3b28a1e89e6fd0
joy293 <opendoor2026@gmail.com>
2024-12-01 16:43:12 +0900

    feat: 캘린더에서 선택한 촬영날짜로 UI update

--- presentation/src/main/java/com/example/presentation/main/view/fragment/CalendarFragment.kt
+++ presentation/src/main/java/com/example/presentation/main/view/fragment/CalendarFragment.kt
@@ -8,17 +8,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.GridLayout
-import androidx.fragment.app.viewModels
+import androidx.fragment.app.activityViewModels
 import com.example.presentation.databinding.FragmentCalendarBinding
 import com.example.presentation.main.vm.ProductDetailViewModel
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.android.material.button.MaterialButton
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
 
 class CalendarFragment : BottomSheetDialogFragment() {
     private var _binding: FragmentCalendarBinding? = null
     private val binding get() = _binding!!
-    private var selectedDay: Calendar? = null
-    private val viewModel: ProductDetailViewModel by viewModels()
+    private val sharedViewModel: ProductDetailViewModel by activityViewModels()
+    private val buttonCount: Int = 9
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,10 +35,10 @@ class CalendarFragment : BottomSheetDialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        makeReservationTimeButton(requireContext())
+        setupTimeButtonCount(requireContext())
         setupCalendarMinDate()
         setupDoneButton()
-        setupCalendar()
+        updateSelectedDay()
     }
 
     override fun onDestroyView() {
@@ -44,31 +48,38 @@ class CalendarFragment : BottomSheetDialogFragment() {
 
     override fun onDismiss(dialog: DialogInterface) {
         super.onDismiss(dialog)
-        val parentFragment = parentFragment as? ProductDetailViewFragment
-        parentFragment?.onDateSelected(true)
+        parentUIUpdate()
     }
 
-    private fun makeReservationTimeButton(context: Context) {
-        val buttonCount = 9
-
+    private fun setupTimeButtonCount(context: Context) {
         val gridLayout = binding.layoutButtonContainer
         for (i in 1..buttonCount) {
-            val button =  MaterialButton(context).apply {
-                id = View.generateViewId()
-                text = "0$i:00"
-                layoutParams = GridLayout.LayoutParams().apply {
-                    height = GridLayout.LayoutParams.WRAP_CONTENT
-                    columnSpec = GridLayout.spec(GridLayout.UNDEFINED, 1f)
-                    rowSpec = GridLayout.spec(GridLayout.UNDEFINED, 1f)
-                    setMargins(8, 8, 8, 8)
-                }
-            }
-            button.setOnClickListener {
-                viewModel.selectedTime.value = button.id.toString()
-                viewModel.setSelectedTime(true)
-            }
+            val button = createTimeButton(context, i)
             gridLayout.addView(button)
         }
+    }
+
+    private fun createTimeButton(context: Context, hour: Int): MaterialButton {
+        val timeFormatter = DateTimeFormatter.ofPattern("HH:mm")
+        val time = LocalTime.of(hour, 0)
+        val buttonText = time.format(timeFormatter)
+
+        val button = MaterialButton(context).apply {
+            id = View.generateViewId()
+            text = buttonText
+            layoutParams = GridLayout.LayoutParams().apply {
+                height = GridLayout.LayoutParams.WRAP_CONTENT
+                columnSpec = GridLayout.spec(GridLayout.UNDEFINED, 1f)
+                rowSpec = GridLayout.spec(GridLayout.UNDEFINED, 1f)
+                setMargins(8, 8, 8, 8)
+            }
+        }
+
+        button.setOnClickListener {
+            updateSelectedTime(button)
+        }
+
+        return button
     }
 
     private fun setupCalendarMinDate() {
@@ -76,27 +87,54 @@ class CalendarFragment : BottomSheetDialogFragment() {
         binding.reservationCalendar.minDate = today
     }
 
-    private fun setupCalendar() {
+
+    private fun updateSelectedTime(button: MaterialButton) {
+        val timeFormatter = DateTimeFormatter.ofPattern("HH:mm")
+        val selectedTime = LocalTime.parse(button.text, timeFormatter)
+
+        sharedViewModel.setSelectedTime(selectedTime)
+        updateEnable()
+    }
+
+    private fun updateSelectedDay() {
         binding.reservationCalendar.setOnDateChangeListener { view, year, month, day ->
-            selectedDay = Calendar.getInstance().apply {
-                set(year, month + 1, day)
-            }
-            if(selectedDay != null) {
-                viewModel.selectedDay.value = selectedDay
-            }
-            hasSelectedDay()
+            val selectedDay = LocalDate.of(year, month + 1, day)
+            sharedViewModel.setSelectedDay(selectedDay)
+
+            showTimeButton()
         }
     }
 
-    private fun hasSelectedDay() {
+    private fun showTimeButton() {
         binding.layoutButtonContainer.visibility = View.VISIBLE
+    }
+
+    private fun updateEnable() {
         binding.btDone.isEnabled = true
     }
 
     private fun setupDoneButton() {
         binding.btDone.setOnClickListener {
-            viewModel.setSelectedDay(true)
+            sharedViewModel.setSelectedDateTime()
+            sharedViewModel.setHasSelectDateTime(binding.btDone.isEnabled)
             dismiss()
         }
+    }
+
+    private fun getDateTimeToString(): String {
+        val dateTime = sharedViewModel.reservationState.value?.dateTime
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
+
+        return if (dateTime != null) {
+            dateTime.format(formatter)
+        } else {
+            "선택한 날짜가 없습니다"
+        }
+    }
+
+    private fun parentUIUpdate() {
+        val parentFragment = parentFragment as? ProductDetailViewFragment
+        parentFragment?.updateButtonEnable(true)
+        parentFragment?.updateText(getDateTimeToString())
     }
 }
--- presentation/src/main/java/com/example/presentation/main/view/fragment/ProductDetailViewFragment.kt
+++ presentation/src/main/java/com/example/presentation/main/view/fragment/ProductDetailViewFragment.kt
@@ -1,6 +1,7 @@
 package com.example.presentation.main.view.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,13 +11,14 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import com.example.presentation.databinding.FragmentProductDetailViewBinding
 import com.example.presentation.main.vm.ProductDetailViewModel
+import com.example.presentation.main.vm.model.ReservationState
+import java.time.LocalDate
 
 class ProductDetailViewFragment : Fragment() {
 
     private var _binding: FragmentProductDetailViewBinding? = null
     private val binding get() = _binding!!
     private val viewModel: ProductDetailViewModel by viewModels()
-
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,9 +27,15 @@ class ProductDetailViewFragment : Fragment() {
     ): View {
         _binding = FragmentProductDetailViewBinding.inflate(inflater, container, false)
         showCalendarBottomSheet()
+        showChangeCalendarBottomSheet()
         isGroup()
-        observeOrderButton()
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        observeReservationState()
+        observeOrderButtonEnable()
     }
 
     override fun onDestroyView() {
@@ -42,6 +50,13 @@ class ProductDetailViewFragment : Fragment() {
         }
     }
 
+    private fun showChangeCalendarBottomSheet() {
+        binding.btChangeReservationDay.setOnClickListener {
+            val bottomSheet = CalendarFragment()
+            bottomSheet.show(childFragmentManager, bottomSheet.tag)
+        }
+    }
+
     private fun isGroup() {
         binding.checkbox1.setOnCheckedChangeListener { _, isGroup ->
             binding.layoutGuest.isVisible = isGroup
@@ -49,13 +64,29 @@ class ProductDetailViewFragment : Fragment() {
         }
     }
 
-    private fun observeOrderButton() {
-        viewModel.isEnabled.observe(viewLifecycleOwner, Observer { isEnabled ->
-            binding.btOrder.isEnabled = isEnabled
-        })
+    private fun observeReservationState() {
+        viewModel.reservationState.observe(viewLifecycleOwner) { reservationState ->
+        }
     }
 
-    fun onDateSelected(isEnabled: Boolean) {
+    private fun observeOrderButtonEnable() {
+        viewModel.isEnabled.observe(viewLifecycleOwner) {
+            binding.btOrder.isEnabled = it
+        }
+    }
+
+    fun updateText(text: String) {
+        binding.tvSelectedDate.text = text
+    }
+
+    fun updateButtonEnable(isEnabled: Boolean) {
         viewModel.setButtonEnabled(isEnabled)
+        toggleVisibility(isEnabled)
+    }
+
+    private fun toggleVisibility(boolean: Boolean) {
+        binding.btSelectReservationDay.isVisible = !boolean
+        binding.tvSelectedDate.isVisible = boolean
+        binding.btChangeReservationDay.isVisible = boolean
     }
 }
