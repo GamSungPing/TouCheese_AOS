758143753f4ce61e6948e9f4c56f80c942c9f9a7
joy293 <opendoor2026@gmail.com>
2024-12-01 20:27:04 +0900

    feat : DTO에 isGroup 추가

--- data/src/main/java/com/example/data/dto/response/product/Data.kt
+++ data/src/main/java/com/example/data/dto/response/product/Data.kt
@@ -1,10 +1,14 @@
 package com.example.data.dto.response.product
 
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
 internal data class Data (
     val id: String,
     val name: String,
     val description: String,
     val imageString: String,
     val price: Int,
-    val reviewCount: Int
+    val reviewCount: Int,
+    val isGroup: Boolean
 )
--- data/src/main/java/com/example/data/dto/response/product/ProductResponse.kt
+++ data/src/main/java/com/example/data/dto/response/product/ProductResponse.kt
@@ -1,5 +1,6 @@
 package com.example.data.dto.response.product
 
+import com.example.domain.model.Product
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
@@ -7,4 +8,16 @@ internal data class ProductResponse(
     val data: Data,
     val msg: String,
     val statusCode: Int
-)
+) {
+    fun toDomainModel(): Product {
+        return Product(
+            id = data.id,
+            name = data.name,
+            description = data.description,
+            imageString = data.imageString,
+            price = data.price,
+            reviewCount = data.reviewCount,
+            isGroup = data.isGroup
+        )
+    }
+}
