43022af4a5852b8ad91796b54ad601f4657cdfe8
joy293 <opendoor2026@gmail.com>
2024-12-07 23:26:50 +0900

    feat: 예약취소 dialog 추가

--- presentation/src/main/java/com/example/presentation/main/view/fragment/ReservationDetailFragment.kt
+++ presentation/src/main/java/com/example/presentation/main/view/fragment/ReservationDetailFragment.kt
@@ -1,5 +1,6 @@
 package com.example.presentation.main.view.fragment
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
@@ -16,6 +17,7 @@ class ReservationDetailFragment : Fragment(R.layout.fragment_reservation_detail)
 
         val binding = FragmentReservationDetailBinding.bind(view)
         setToolbar(binding)
+        setCancelDialog(binding)
     }
 
     private fun setToolbar(binding: FragmentReservationDetailBinding) {
@@ -23,5 +25,25 @@ class ReservationDetailFragment : Fragment(R.layout.fragment_reservation_detail)
             val navController = findNavController()
             navController.navigate(R.id.action_reservationDetailFragment_to_reservationFragment)
         }
+    }
+
+    private fun setCancelDialog(binding: FragmentReservationDetailBinding) {
+        binding.btCancelReservation.setOnClickListener {
+            val builder: AlertDialog.Builder = AlertDialog.Builder(context)
+            builder
+                .setTitle("예약 취소")
+                .setMessage("예약을 취소하면 복구할 수 없습니다")
+                .setPositiveButton("확인") { dialog, which ->
+
+                }
+                .setNegativeButton("취소") { dialog, which ->
+
+                }
+
+
+            val dialog: AlertDialog = builder.create()
+            dialog.show()
+        }
+
     }
 }
