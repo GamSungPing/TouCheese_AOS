7c6241274df50e6a46904780baff0262f65e9ba0
joy293 <opendoor2026@gmail.com>
2024-12-08 22:18:03 +0900

    feat: 예약 api data UI 반영

--- presentation/src/main/java/com/example/presentation/main/view/adapter/ReservationViewAdapter.kt
+++ presentation/src/main/java/com/example/presentation/main/view/adapter/ReservationViewAdapter.kt
@@ -4,11 +4,12 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
+import com.example.domain.model.Reservation
 import com.example.presentation.databinding.ItemReservationBinding
 
 class ReservationViewAdapter(
     private val onClickReservation: (Int) -> Unit
-) : ListAdapter<String, ReservationViewHolder>(ReservationDiffCallback) {
+) : ListAdapter<Reservation, ReservationViewHolder>(ReservationDiffCallback) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReservationViewHolder {
         return ReservationViewHolder(
             ItemReservationBinding.inflate(LayoutInflater.from(parent.context), parent, false),
@@ -22,18 +23,17 @@ class ReservationViewAdapter(
     }
 
     companion object {
-        private val ReservationDiffCallback = object : DiffUtil.ItemCallback<String>() {
+        private val ReservationDiffCallback = object : DiffUtil.ItemCallback<Reservation>() {
             override fun areItemsTheSame(
-                oldItem: String,
-                newItem: String
+                oldItem: Reservation,
+                newItem: Reservation
             ): Boolean {
-//                return oldItem.id == newItem.id
-                return oldItem == newItem
+                return oldItem.id == newItem.id
             }
 
             override fun areContentsTheSame(
-                oldItem: String,
-                newItem: String
+                oldItem: Reservation,
+                newItem: Reservation
             ): Boolean {
                 return oldItem == newItem
             }
--- presentation/src/main/java/com/example/presentation/main/view/adapter/ReservationViewHolder.kt
+++ presentation/src/main/java/com/example/presentation/main/view/adapter/ReservationViewHolder.kt
@@ -1,7 +1,10 @@
 package com.example.presentation.main.view.adapter
 
 import androidx.recyclerview.widget.RecyclerView
+import com.example.domain.model.Reservation
 import com.example.presentation.databinding.ItemReservationBinding
+import com.example.presentation.main.vm.model.ReservationStatus
+import com.example.presentation.util.ext.setImage
 
 class ReservationViewHolder (
     private val binding: ItemReservationBinding,
@@ -12,8 +15,16 @@ class ReservationViewHolder (
         setReservationOnClickListener()
     }
 
-    fun bind(item: String) {
-        binding.tvReservationStudio.text = item
+    fun bind(reservation: Reservation) {
+        binding.tvReservationStudio.text = reservation.studioName
+        binding.tvReservationState.text = reservation.reservationStatus
+        binding.tvReservationDateTime.text = reservation.reservationTime
+
+        val studioImage = binding.ivCard
+        studioImage.setImage(studioImage, reservation.studioImage)
+
+        val reservationStatus = ReservationStatus.fromString(reservation.reservationStatus)
+        binding.tvReservationState.text = reservationStatus.status
     }
 
     private fun setReservationOnClickListener() {
