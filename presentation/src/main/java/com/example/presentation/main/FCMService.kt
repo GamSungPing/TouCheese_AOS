aa1733229acb4dbda586cf1d4f622771f3421032
joy293 <opendoor2026@gmail.com>
2024-12-11 14:08:56 +0900

    feat: design icon, color 추가

--- presentation/src/main/java/com/example/presentation/main/FCMService.kt
+++ presentation/src/main/java/com/example/presentation/main/FCMService.kt
@@ -1,9 +1,11 @@
-package com.example.data.service
+package com.example.presentation.main.view.fragment
 
 import android.content.ContentValues
 import android.util.Log
+import com.example.presentation.main.view.MainActivity.NotificationHelper.sendNotification
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
 class FCMService : FirebaseMessagingService() {
 
@@ -19,5 +21,14 @@ class FCMService : FirebaseMessagingService() {
 
     override fun onNewToken(token: String) {
         super.onNewToken(token)
+    }
+
+    // 메시지 수신 시 호출
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        remoteMessage.notification?.let {
+            val title = it.title ?: "Default Title"
+            val message = it.body ?: "Default Body"
+            sendNotification(applicationContext, title, message)
+        }
     }
 }
