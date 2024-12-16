3d72d4022978f594c83b4b6c494f882461ddaf70
chanho0908 <uni070@naver.com>
2024-12-17 04:19:24 +0900

    feat : 예약 가능 시간 정보 Domain Model

--- domain/src/main/java/com/example/domain/model/ProductOption.kt
+++ domain/src/main/java/com/example/domain/model/ProductOption.kt
@@ -8,3 +8,9 @@ data class ProductOption (
 ){
     val optionMessage get() = optionFormat(name, price)
 }
+
+val FakeProductOption = listOf(
+    ProductOption("보정 사진 추가", 100000),
+    ProductOption("원본 전체 받기", 100000),
+    ProductOption("액자 프린팅", 100000)
+)
