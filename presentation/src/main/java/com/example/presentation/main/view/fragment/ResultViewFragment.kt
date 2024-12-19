11d9ebd3ca6df119d41675087600f817835b8769
joy293 <opendoor2026@gmail.com>
2024-12-19 14:33:48 +0900

    feat: 북마크 변경, 포트폴리오 크기 수정

--- presentation/src/main/java/com/example/presentation/main/view/fragment/ResultViewFragment.kt
+++ presentation/src/main/java/com/example/presentation/main/view/fragment/ResultViewFragment.kt
@@ -8,9 +8,7 @@ import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import androidx.navigation.fragment.navArgs
-import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.domain.rule.Pricing
 import com.example.domain.rule.Region
@@ -63,12 +61,6 @@ class ResultViewFragment : Fragment(R.layout.fragment_result_view) {
             layoutManager =
                 LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
             itemAnimator = null
-            addItemDecoration(
-                DividerItemDecoration(
-                    requireContext(),
-                    DividerItemDecoration.VERTICAL
-                )
-            )
             adapter = resultViewAdapter
         }
 
