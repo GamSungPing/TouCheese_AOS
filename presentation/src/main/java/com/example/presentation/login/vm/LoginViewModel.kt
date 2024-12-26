da54404abe0b45768d7e3d27411a087654a6492d
chanho0908 <uni070@naver.com>
2024-12-26 15:53:39 +0900

    chore : 로그삭제

--- presentation/src/main/java/com/example/presentation/login/vm/LoginViewModel.kt
+++ presentation/src/main/java/com/example/presentation/login/vm/LoginViewModel.kt
@@ -1,18 +1,15 @@
 package com.example.presentation.login.vm
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.domain.repository.AuthRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -32,7 +29,6 @@ class LoginViewModel @Inject constructor(
         viewModelScope.launch {
             authRepository.loggedIn
                 .map { isLoggedIn ->
-                    Log.d("dasdas", "$isLoggedIn")
                     LoginState(
                         loginEvent = if (isLoggedIn) LoginEvent.LoggedIn else LoginEvent.LoginRequired
                     )
